0864a70c5ea59040c1b0bb8d2b99643444a6dc0d
salgarsanju@gmail.com <salgarsanju@gmail.com>
2025-09-20 16:16:26 +0530

    Entity class column added

--- crud/src/main/java/com/youtube/crud/entity/Employee.java
+++ crud/src/main/java/com/youtube/crud/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.youtube.crud.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,17 +15,17 @@ public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer employeeId;
-	
+	@Column
 	private String employeeName;
-	
+	@Column
 	private String employeeContactNumber;
-	
+	@Column
 	private String employeeAddress;
-	
+	@Column
 	private String employeeGender;
-	
+	@Column
 	private String employeeDepartment;
-	
+	@Column
 	private String employeeSkill;
 
 }
